e82c0faff7f74ce8cfb53adea31d5bbe9235d79f
matheusvoi-dev <matheus.voi@escola.pr.gov.br>
2026-09-09 13:45:14 +0000

    a

---
+++ aula.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+public class aula{
+    public static void main(String[] args) {
+    ArrayList<String> carrinho = new ArrayList<>();
+        carrinho.add("Notebook");
+        carrinho.add("Mouse");
+        carrinho.add("Teclado");
+        carrinho.add("Monitor");
+        carrinho.add("Fone");
+        System.out.println(carrinho);
+    }
+}

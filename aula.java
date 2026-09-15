7b4f9bc94dd983963ee8578c465f36ecb6f935ca
matheusvoi-dev <matheus.voi@escola.pr.gov.br>
2026-09-15 15:11:27 +0000

    e

--- aula.java
+++ aula.java
@@ -7,12 +7,15 @@ public class aula{
         carrinho.add("Teclado");
         carrinho.add("Monitor");
         carrinho.add("Fone");
-        carrinho.remove("Mouse");
-        System.out.println("Primeiro produto: " + carrinho.get(0));
+        System.out.println("Primeiro produto " + carrinho.get(0));
         System.out.println("Segundo produto: " + carrinho.get(1));
         System.out.println("Terceiro produto: " + carrinho.get(2));
         System.out.println("Quarto produto: " + carrinho.get(3));
         System.out.println("Quinto produto: " + carrinho.get(4));
+        carrinho.remove("Mouse");
         System.out.println("Depois de remover: " + carrinho);
+        System.out.println("Você tem " + carrinho.size() + " itens no carrinho");
+     
     }
 }
+        
---
+++ aula2.java
@@ -0,0 +1,8 @@
+public class aula2{
+    public static void main(String[] args) {
+        String[] contatosDaAna = {"Pai: 41999990000", "Mãe: 41988880000", "Tia:41999999999"};
+        for (String contato : contatosDaAna)
+        System.out.println(contato);
+    }
+}
+        

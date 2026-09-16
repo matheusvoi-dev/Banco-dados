58c5ecee57279b367cda16102a21beadafbce4d6
matheusvoi-dev <matheus.voi@escola.pr.gov.br>
2026-09-16 15:09:06 +0000

    f

---
+++ back/aula3.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+public class aula3{
+    public static void main(String[] args) {
+       HashMap<Integer, String> produtos = new HashMap<>();
+        produtos.put(101, "Notebook");
+        produtos.put(102, "Mouse");
+        produtos.put(103, "Teclado");
+        System.out.println(produtos);
+    }
+}

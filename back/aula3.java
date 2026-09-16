5f1df470a07635e670895db09cd07b73fa86a431
matheusvoi-dev <matheus.voi@escola.pr.gov.br>
2026-09-16 15:27:33 +0000

    g

--- back/aula3.java
+++ back/aula3.java
@@ -5,6 +5,20 @@ public class aula3{
         produtos.put(101, "Notebook");
         produtos.put(102, "Mouse");
         produtos.put(103, "Teclado");
+        produtos.put(108, "Base de carregamento");
+        produtos.put(300, "Mousepad");
+        produtos.put(100, "Webcam");
+        produtos.put(240, "Fone");
+        produtos.put(250, "Action figure");
         System.out.println(produtos);
+        System.out.println("Produto 102 é o: " + produtos.get(102));
+        System.out.println("Produto 100 é o: " + produtos.get(100));
+           if (produtos.containsKey(400)) {
+        System.out.println("Produto: " + produtos.get(400));
+        } else {
+        System.out.println("Produto não encontrado!");
+        }
+        produtos.remove(300);
+        System.out.println("Depois de remover: " + produtos);
     }
 }
